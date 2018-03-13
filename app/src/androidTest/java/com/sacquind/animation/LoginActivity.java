481dfdfd1d7a67572cf12fb9ae48cd282527c853
Mouallez <5V43r2Vm>
2018-03-13 11:40:57 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/sacquind/animation/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.sacquind.animation;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class LoginActivity extends ApplicationTestCase<Application> {
+    public LoginActivity() {
+        super(Application.class);
+    }
+}
